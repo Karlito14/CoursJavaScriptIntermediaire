e1690ee22f08ff9e8fb32cdf9ef55856e46f0995
Karlito14 <leiroz26@hotmail.com>
2022-12-20 17:44:59 +0100

    Exercices du cours 1

--- cours/01 - Raccourcis d'operateurs, puissance et modulo/exercices.js
+++ cours/01 - Raccourcis d'operateurs, puissance et modulo/exercices.js
@@ -24,15 +24,43 @@
 // Exercice 0
 // Déclarez une fonction convertirEnSemainesEtJours(nombreDeJours) et faites lui retourner
 // une template string qui correspond à la solution de l'énoncé
+function convertirEnSemainesEtJours(nombreDeJours){
+    let joursRestants = nombreDeJours % 7;
+    let nbreSemaines = (nombreDeJours - joursRestants) / 7;
+    return `${nombreDeJours} jour(s), c'est équivalent à ${nbreSemaines} semaine(s) et ${joursRestants} jour(s)`;
+}
 
 // Exercice 1
 // Déclarez une fonction calculPrixNappes(nombreDeTables, diametreTable, prixTissu)
 // qui renvoie le prix total du tissu à acheter pour faire les nappes en fonction des tables utilisées
+function calculPrixNappes(nombreDeTables, diametreTable, prixTissu){
+    let surface = diametreTable ** 2;
+    let surfaceTotal = surface * nombreDeTables;
+    return surfaceTotal * prixTissu;
+}
 
 
 // Exercice 2
 // Déclarez une fonction calculPrixNappesDegressif(nombreDeTables, diametreTable, prixTissu)
 // qui renvoie le prix total du tissu à acheter pour faire les nappes en prenant en compte le tarif dégressif
+function calculPrixNappesDegressif(nombreDeTables, diametreTable, prixTissu){
+    let surface = diametreTable ** 2;
+    let surfaceTotal = surface * nombreDeTables;
+    let prixTotal = surfaceTotal * prixTissu;
+    if(surfaceTotal >= 100){
+        prixTotal = prixTotal - prixTotal * 0.5;
+    }
+    if(surfaceTotal >= 50 && surfaceTotal < 100){
+        prixTotal = prixTotal - prixTotal * 0.3;
+    }
+    if(surfaceTotal >= 30 && surfaceTotal < 50){
+        prixTotal = prixTotal - prixTotal * 0.2;
+    }
+    if(surfaceTotal >= 20 && surfaceTotal < 30){
+        prixTotal = prixTotal - prixTotal * 0.1;
+    }
+    return prixTotal;
+}
 
 
 // Exercice 3
@@ -41,10 +69,10 @@
 
 // Chassez le bogue 0
 // Pour faire cet exercice décommentez le code de Tom ci-dessous
-// function repeter(mot, nombreDeRepetitions) {
-//   let motRepete = mot;
-//   while (nombreDeRepetitions-- !== 0) {
-//     motRepete += mot;
-//   }
-//   return motRepete;
-// }
+function repeter(mot, nombreDeRepetitions) {
+   let motRepete = mot;
+   while (--nombreDeRepetitions !== 0) {
+     motRepete += mot;
+   }
+   return motRepete;
+}
